101365640fcf011d9d1c42be1d7af0bed6ff8850
iPablo <pabloig90@gmail.com>
2017-03-08 09:55:47 +0100

    Commit #03: Added feature - Post list

--- python_sevilla/python_sevilla/urls.py
+++ python_sevilla/python_sevilla/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^posts/$', views.PostsView.as_view(), name='posts_lists'),
     url(r'^posts/create/$', views.PostCreateView.as_view(), name='post_create'),
+    url(r'^posts/(?P<pk>[-\w]+)/$', views.PostDetailView.as_view(), name='post_detail')
 ]
 
